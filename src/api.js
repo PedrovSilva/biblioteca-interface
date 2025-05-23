414b49256a42c48c3f09c493a611dd9c8c650e50
Pedro Vitor <82241565+PedrovSilva@users.noreply.github.com>
2025-05-23 15:24:38 +0000

    correcoes

--- src/api.js
+++ src/api.js
@@ -2,10 +2,12 @@ import axios from 'axios'
 
 const API_BASE = process.env.REACT_APP_API_BASE_URL
 
-export const listarLivros = () => axios.get(API_BASE)
-export const cadastrarLivro = (formData) => axios.post(API_BASE, formData, {
+const API_LIVROS = API_BASE + "/livros"
+
+export const listarLivros = () => axios.get(API_LIVROS)
+export const cadastrarLivro = (formData) => axios.post(API_LIVROS, formData, {
   headers: { 'Content-Type': 'multipart/form-data' }
 })
-export const obterLivro = (id) => axios.get(`${API_BASE}/${id}`)
-export const listarGeneros  =  axios.get(`${API_BASE}/generos`)
-export const baixarPdf = (id) => axios.get(`${API_BASE}/${id}/pdf`, { responseType: 'blob' })
+export const obterLivro = (id) => axios.get(`${API_LIVROS}/${id}`)
+export const listarGeneros = () => axios.get(`${API_BASE}/generos`);
+export const baixarPdf = (id) => axios.get(`${API_LIVROS}/${id}/pdf`, { responseType: 'blob' })
